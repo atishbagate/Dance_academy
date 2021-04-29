8d3cb738b6b6db8404e3211596b8c534f69c6f4f
atish <bagateatish@gmail.com>
2021-04-29 13:37:08 +0530

    this is 1st commit

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded()) //express.urlencoded() function is a built-in midd
 //A template engine enables you to use static template files in your application. 
 // At runtime, the template engine replaces variables in a template file with actual values, 
 // and transforms the template into an HTML file sent to the client. 
-// This approach makes it easier to design an HTML page.
+// This approach makes it easier to design an 
 
 //set the views directory
 app.set('view engine','pug'); //set the template as pug
